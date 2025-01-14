730c4d8f48ae37bb49b3d1c94ea937fc2f39adb6
Panagiotis Mourlas <panamour25@gmail.com>
2025-01-14 17:57:05 +0200

    Added Generated Basic Grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
 {
     public GameObject gridCellPrefab;
-    public Transform gridPanel;
+    public RectTransform gridPanel;
+    public GridLayoutGroup gridLayoutGroup;
     public int gridSize = 5;
 
     void Start()
     {
+        AdjustCellSize(gridSize, gridSize);
         GenerateGrid(gridSize, gridSize);
+    }
+
+    void AdjustCellSize(int rows, int columns)
+    {
+        float panelWidth = gridPanel.rect.width;
+        float panelHeight = gridPanel.rect.height;
+
+        float cellWidth = (panelWidth - (gridLayoutGroup.spacing.x * (columns - 1))) / columns;
+        float cellHeight = (panelHeight - (gridLayoutGroup.spacing.y * (rows - 1))) / rows;
+
+        gridLayoutGroup.cellSize = new Vector2(cellWidth, cellHeight);
     }
 
     void GenerateGrid(int rows, int columns)
