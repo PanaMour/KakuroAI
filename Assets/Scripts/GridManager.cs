ea2410e68daa76edc3b6f5098dbebcac18d35fb2
Panagiotis Mourlas <panamour25@gmail.com>
2025-01-22 15:08:54 +0200

    Added some Grid Logic

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,13 +5,25 @@ using UnityEngine.UI;
 public class GridManager : MonoBehaviour
 {
     public GameObject gridCellPrefab;
+    public GameObject cluePrefab;
     public RectTransform gridPanel;
+    public RectTransform topCluesContainer;
+    public RectTransform leftCluesContainer;
     public GridLayoutGroup gridLayoutGroup;
     public int gridSize = 5;
 
+    private int[,] gridData;
+    private int[] topClues;
+    private int[] leftClues;
+
     void Start()
     {
+        gridData = new int[gridSize, gridSize];
+        topClues = new int[gridSize];
+        leftClues = new int[gridSize];
+
         AdjustCellSize(gridSize, gridSize);
+        GenerateClues(gridSize, gridSize);
         GenerateGrid(gridSize, gridSize);
     }
 
@@ -26,6 +38,31 @@ public class GridManager : MonoBehaviour
         gridLayoutGroup.cellSize = new Vector2(cellWidth, cellHeight);
     }
 
+    void GenerateClues(int rows, int columns)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            GameObject topClue = Instantiate(cluePrefab, topCluesContainer);
+            TextMeshProUGUI clueText = topClue.GetComponent<TextMeshProUGUI>();
+            if (clueText != null)
+            {
+                topClues[j] = Random.Range(10, 30);
+                clueText.text = topClues[j].ToString();
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            GameObject leftClue = Instantiate(cluePrefab, leftCluesContainer);
+            TextMeshProUGUI clueText = leftClue.GetComponent<TextMeshProUGUI>();
+            if (clueText != null)
+            {
+                leftClues[i] = Random.Range(10, 30);
+                clueText.text = leftClues[i].ToString();
+            }
+        }
+    }
+
     void GenerateGrid(int rows, int columns)
     {
         for (int i = 0; i < rows; i++)
@@ -34,12 +71,114 @@ public class GridManager : MonoBehaviour
             {
                 GameObject newCell = Instantiate(gridCellPrefab, gridPanel);
 
-                TextMeshProUGUI cellText = newCell.GetComponentInChildren<TextMeshProUGUI>();
-                if (cellText != null)
+                TMP_InputField inputField = newCell.GetComponentInChildren<TMP_InputField>();
+                if (inputField != null)
                 {
-                    cellText.text = Random.Range(1, 10).ToString();
+                    int row = i;
+                    int column = j;
+
+                    inputField.onEndEdit.AddListener(value =>
+                    {
+                        int parsedValue;
+                        if (int.TryParse(value, out parsedValue) && parsedValue >= 1 && parsedValue <= 9)
+                        {
+                            gridData[row, column] = parsedValue;
+                            Debug.Log($"Updated gridData[{row}, {column}] = {parsedValue}");
+                            ValidateGrid();
+                        }
+                        else
+                        {
+                            Debug.Log($"Invalid input at [{row}, {column}]. Resetting.");
+                            inputField.text = "";
+                            gridData[row, column] = 0;
+                        }
+                    });
                 }
             }
         }
     }
+
+    void ValidateGrid()
+    {
+        bool[] rowValidity = new bool[gridSize];
+        bool[] columnValidity = new bool[gridSize];
+
+        for (int i = 0; i < gridSize; i++)
+        {
+            int rowSum = 0;
+            for (int j = 0; j < gridSize; j++)
+            {
+                rowSum += gridData[i, j];
+            }
+            rowValidity[i] = rowSum == leftClues[i];
+        }
+
+        for (int j = 0; j < gridSize; j++)
+        {
+            int columnSum = 0;
+            for (int i = 0; i < gridSize; i++)
+            {
+                columnSum += gridData[i, j];
+            }
+            columnValidity[j] = columnSum == topClues[j];
+        }
+
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                int cellIndex = i * gridSize + j;
+                Transform cell = gridPanel.GetChild(cellIndex);
+                Image cellImage = cell.GetComponent<Image>();
+
+                if (cellImage != null)
+                {
+                    if (gridData[i, j] == 0)
+                    {
+                        cellImage.color = Color.white;
+                    }
+                    else
+                    {
+                        bool isValid = rowValidity[i] && columnValidity[j];
+                        cellImage.color = isValid ? Color.green : Color.red;
+                    }
+                }
+            }
+        }
+    }
+
+
+    void HighlightRow(int rowIndex, bool isValid)
+    {
+        for (int j = 0; j < gridSize; j++)
+        {
+            int cellIndex = rowIndex * gridSize + j;
+            Debug.Log($"cellindex row = {cellIndex} , {isValid}");
+            Transform cell = gridPanel.GetChild(cellIndex);
+            Image cellImage = cell.GetComponent<Image>();
+
+            if (cellImage != null)
+            {
+                cellImage.color = isValid ? Color.green : Color.red;
+            }
+        }
+    }
+
+
+    void HighlightColumn(int columnIndex, bool isValid)
+    {
+        for (int i = 0; i < gridSize; i++)
+        {
+            int cellIndex = i * gridSize + columnIndex;
+            Debug.Log($"Index col = {cellIndex} , {isValid}");
+            Transform cell = gridPanel.GetChild(cellIndex);
+            Image cellImage = cell.GetComponent<Image>();
+
+            if (cellImage != null)
+            {
+                cellImage.color = isValid ? Color.green : Color.red;
+            }
+        }
+    }
+
 }
